30f4b570b76dae544295a4788643723fcf7b294a
Hisashi Goto <starbucks1coffee@gmail.com>
2021-01-31 08:55:11 +0900

    Create insert_directmethod_call02.js
    // createFromIotHubConnectionString 関数のコード行 eventHubClient = client; の後に
    // sendDirectMethod 関数の呼び出しを追加します。

---
+++ receiver/insert_directmethod_call02.js
@@ -0,0 +1,9 @@
+//
+// コード挿入箇所
+// createFromIotHubConnectionString 関数のコード行 eventHubClient = client; の後に
+// sendDirectMethod 関数の呼び出しを追加します。
+//
+// Send a direct method to turn the fan on.
+    sendDirectMethod();
+    
+    
